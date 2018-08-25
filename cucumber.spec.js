87e528bf6cbc92afc67ccae1ee5bf1c82060f43e
Matt Wynne <matt@mattwynne.net>
2018-08-25 18:59:03 +0100

    Add a test for not resetting perspectives

--- cucumber.spec.js
+++ cucumber.spec.js
@@ -38,14 +38,6 @@ describe('cucumber integration', () => {
     sinon.assert.calledOnce(login)
   })
 
-  it('resets the state between scenarios', async () => {
-    const { actor, reset } = require('./cucumber')
-    const dave = await actor('dave')
-    reset()
-    const anotherDave = await actor('dave')
-    assert.notDeepEqual(dave, anotherDave)
-  })
-
   it('creates and stores perspectives by name', () => {
     const createUser = Task('create user')
     const { perspective } = require('./cucumber')
@@ -68,4 +60,24 @@ describe('cucumber integration', () => {
     await dave.attemptsTo(createUser)
     sinon.assert.called(browser.click)
   })
+
+  it('resets the state between scenarios', async () => {
+    const { actor, reset } = require('./cucumber')
+    const dave = await actor('dave')
+    reset()
+    const anotherDave = await actor('dave')
+    assert.notDeepEqual(dave, anotherDave)
+  })
+
+  it('does not reset perspectives between scenarios', () => {
+    const createUser = Task('create user')
+    const { perspective } = require('./cucumber')
+    const onePerspecrtive = perspective('web', handle => {
+      handle(createUser, () => ({ browser }) => browser.click())
+    })
+    reset()
+    const anotherPerspective = perspective('web')
+    assert.deepEqual(onePerspecrtive, anotherPerspective)
+  })
+
 })
