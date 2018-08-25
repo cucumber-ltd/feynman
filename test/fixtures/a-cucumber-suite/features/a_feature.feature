264dce33c9717192b75ad92c97ebf2444ded9805
Matt Wynne <matt@mattwynne.net>
2018-08-25 19:04:52 +0100

    Add bare bones cucumber integration fixture

---
+++ test/fixtures/a-cucumber-suite/features/a_feature.feature
@@ -0,0 +1,7 @@
+Feature: Bookings
+
+  Scenario: Book a flight
+    When I book a flight
+
+  Scenario: Book a room
+    When I book a room
---
+++ test/fixtures/a-cucumber-suite/features/step_definitions.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { When } = require('cucumber')
+
+When('I book a flight', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+})
+
+When('I book a room', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+})
